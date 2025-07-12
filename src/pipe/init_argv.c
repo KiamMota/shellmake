44e1747d5f8572ff1c4ab3b58f59c14974ae12ca
KiamMota <somentekiam@gmail.com>
2025-07-12 17:24:03 -0300

    add : error ocurrences

--- src/pipe/init_argv.c
+++ src/pipe/init_argv.c
@@ -4,37 +4,81 @@
 // ==> this function gets the filelist getted by the argv and returns a strvec_t and
 // existing files
 
-strvec_t* _validate_existing_files(strvec_t* filelist, char* suffix)
+
+#define MENU \
+"\
+\033[0;32mWelcome to ShellMake\033[0m\n\
+Usage:\n\
+  -> shellmake script.sh [script2.sh ...] [Build.json ...]\n\
+\n\
+Examples:\n\
+  -> shellmake a.sh b.sh c.sh\n\
+  -> shellmake a.sh b.sh Build.json\n\
+  -> shellmake a.sh BuildA.json\n\
+\n\
+|Notes:\n\
+|- At least one .sh file is required.\n\
+|- Each .sh can have an optional matching Build.json.\n\
+|- You can list all .sh first, then .json files, or pair them.\n\
+|- If only one argument is given, this help is shown.\n\
+|===============================================================\n"
+void _menu_help()
 {
+		printf(MENU);
+}
+
+
+BOOL _existing_files(strvec_t* filelist)
+{
+		strvec_t* invalid_files = strvec_alloc();
 		strvec_t* valid_files = strvec_alloc();
-		for(short i =1; i<strvec_get_lines(filelist); i++)
+		for(short i =0; i<strvec_get_lines(filelist); i++)
 		{
-				if(strstr(filelist->_arr[i], suffix))
+				if(f_file_exists(filelist->_arr[i]))
 				{
-						if(f_file_exists(filelist->_arr[i]))
-						{
-								printf("file '%s' exists.\n", filelist->_arr[i]);
-								strvec_insert(valid_files, filelist->_arr[i]);	
-						}
-				}else
-				printf("file '%s' doesn't exists\n", filelist->_arr[i]);
+						strvec_insert(valid_files, filelist->_arr[i]);	
+				}
+				else break; 
 		}
 
-		return valid_files;
+				if(strvec_get_lines(valid_files) == strvec_get_lines(filelist))
+				{
+						strvec_destroy(&filelist);
+						filelist = valid_files;
+						return TRUE;
+				}else {
+						return FALSE;
+				}
+		
 }
 
 void init_argv(int argn, char** argv)
 {
-		strvec_t* filelist = strvec_alloc();
+		if(argn <=1 )
+		{
+				_menu_help(); return;
+		}
+		
+		strvec_t* eph_filelist = strvec_alloc();
+		strvec_t* eph_buildflist = strvec_alloc();
+	
 
 		for(int i =1; i<argn; i++)
 		{
 				if(strstr(argv[i], ".sh"))
-				{
-						strvec_insert(filelist, argv[i]);
-				}
+						strvec_insert(eph_filelist, argv[i]);
+				if(strstr(argv[i], ".json"))
+						strvec_insert(eph_buildflist, argv[i]);
 		}
+		strvec_printall(eph_buildflist);
+		printf("validating shell files...");
+		if(!_existing_files(eph_filelist)) 
+				printf("	fail.\n");
+		else printf("	success.\n");
 
-		strvec_printall(filelist);
+		printf("validating build files...");
+		if(!_existing_files(eph_buildflist))
+				printf("	fail.\n");
+		else printf("	success");
 }
 
